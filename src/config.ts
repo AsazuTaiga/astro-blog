a53e537f53c2386b37df41f9de0c349279bd4841
AsazuTaiga <taiga.asazu@icloud.com>
2022-08-13 21:29:17 +0900

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Something by @AsazuTaiga";
+export const SITE_DESCRIPTION = "@AsazuTaigaの日常や非日常を記録するブログ";
